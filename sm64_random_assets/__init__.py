3c68032062f09fc7883899b56b02105ef8370197
joncrall <erotemic@gmail.com>
2024-10-18 18:21:41 -0400

    Bump version

--- sm64_random_assets/__init__.py
+++ sm64_random_assets/__init__.py
@@ -14,7 +14,7 @@ Relevant files are:
 
 """
 
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 __author__ = 'Jon Crall'
 __author_email__ = 'erotemic@gmail.com'
 __url__ = 'https://github.com/Erotemic/sm64-random-assets'
